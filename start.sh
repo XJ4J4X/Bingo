4b53b61c97e8b2a161419fd60e83a0cb5c03755d
J4X <j4x@j4x.tech>
2026-08-21 22:16:56 +0200

    Nettoyage des fichiers temporaires

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-lsof -i :8080 | grep LISTEN | awk '{print $2}' | xargs kill -9 2>/dev/null || true
-ps aux | grep "[s]erver.py" | awk '{print $2}' | xargs kill -9 2>/dev/null || true
-python3 server.py > server.log 2>&1 &
-echo $! > server.pid
